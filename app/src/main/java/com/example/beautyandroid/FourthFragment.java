82172ad6e0863fa81a4103c88502fdf1f5e9c251
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2022-12-02 08:55:59 +0000

    Login feature navigation implemented

---
+++ app/src/main/java/com/example/beautyandroid/FourthFragment.java
@@ -0,0 +1,36 @@
+package com.example.beautyandroid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.example.beautyandroid.databinding.FragmentFourthBinding;
+
+public class FourthFragment extends Fragment {
+
+    private FragmentFourthBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFourthBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
--- app/src/main/java/com/example/beautyandroid/ThirdFragment.java
+++ app/src/main/java/com/example/beautyandroid/ThirdFragment.java
@@ -7,11 +7,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-import com.example.beautyandroid.databinding.FragmentFirstBinding;
+import com.example.beautyandroid.databinding.FragmentThirdBinding;
 
-public class FirstFragment extends Fragment {
+public class ThirdFragment extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private FragmentThirdBinding binding;
 
     @Override
     public View onCreateView(
@@ -19,7 +19,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -27,19 +27,19 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.choice1First.setOnClickListener(new View.OnClickListener() {
+        binding.backThird.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_FirstFragment);
             }
         });
 
-        binding.choice2First.setOnClickListener(new View.OnClickListener() {
+        binding.confirmThird.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_ThirdFragment);
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_FourthFragment);
             }
         });
     }
