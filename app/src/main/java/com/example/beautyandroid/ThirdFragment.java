bf9075bfbe6bb3d649d39cf39effc3def2af44a7
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2022-12-03 18:03:52 +0000

    Firebase registration added

--- app/src/main/java/com/example/beautyandroid/ThirdFragment.java
+++ app/src/main/java/com/example/beautyandroid/ThirdFragment.java
@@ -2,21 +2,29 @@ package com.beautyorder.androidclient;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.beautyorder.androidclient.databinding.FragmentThirdBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ThirdFragment extends Fragment {
     private FragmentThirdBinding binding;
 
     private EditText email;
     private EditText password;
+    private FirebaseAuth mAuth;
 
     boolean isEmail(EditText text) {
         CharSequence email = text.getText().toString();
@@ -45,6 +53,7 @@ public class ThirdFragment extends Fragment {
 
         email = view.findViewById(R.id.register_email);
         password = view.findViewById(R.id.register_password);
+        mAuth = FirebaseAuth.getInstance();
 
         binding.backThird.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +80,39 @@ public class ThirdFragment extends Fragment {
                 }
 
                 if (navigate) {
-                    NavHostFragment.findNavController(ThirdFragment.this)
-                            .navigate(R.id.action_ThirdFragment_to_FourthFragment);
+
+//                    Auth.auth().createUser(withEmail: username, password: password) { authResult, error in
+//
+//                        if let e = error {
+//                            print(e.localizedDescription)
+//                        } else {
+//                            print("User registration succesful")
+//
+//                            welcomeCallbacks.segueCallback?(K.welcomeToDaySegueIdentifier)
+//                        }
+//                    }
+
+                    String emailText = email.getText().toString();
+                    String passwordText = password.getText().toString();
+
+                    mAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("BeautyOrder", "createUserWithEmail:success");
+
+                                    NavHostFragment.findNavController(ThirdFragment.this)
+                                        .navigate(R.id.action_ThirdFragment_to_FourthFragment);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("BeautyOrder", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(view.getContext(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                    });
                 }
             }
         });
