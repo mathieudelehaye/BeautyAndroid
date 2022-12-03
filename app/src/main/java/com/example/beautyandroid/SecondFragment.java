0bb8c79bbfb17af6f2b05cfee6a04d2a556c192f
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2022-12-03 15:35:15 +0000

    Register and login forms added

--- app/src/main/java/com/example/beautyandroid/SecondFragment.java
+++ app/src/main/java/com/example/beautyandroid/SecondFragment.java
@@ -1,9 +1,12 @@
 package com.example.beautyandroid;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
@@ -12,6 +15,19 @@ import com.example.beautyandroid.databinding.FragmentSecondBinding;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+    private EditText email;
+    private EditText password;
+
+    // TODO: put those methods in a shared module
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
 
     @Override
     public View onCreateView(
@@ -27,6 +43,9 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        email = view.findViewById(R.id.signin_email);
+        password = view.findViewById(R.id.signin_password);
+
         binding.backSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,8 +57,23 @@ public class SecondFragment extends Fragment {
         binding.confirmSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
+
+                Boolean navigate = true;
+
+                if (isEmail(email) == false) {
+                    email.setError("Enter valid email!");
+                    navigate = false;
+                }
+
+                if (isEmpty(password)) {
+                    password.setError("Password is required!");
+                    navigate = false;
+                }
+
+                if (navigate) {
+                    NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FourthFragment);
+                }
             }
         });
     }
--- app/src/main/java/com/example/beautyandroid/ThirdFragment.java
+++ app/src/main/java/com/example/beautyandroid/ThirdFragment.java
@@ -1,17 +1,32 @@
 package com.example.beautyandroid;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.beautyandroid.databinding.FragmentThirdBinding;
 
 public class ThirdFragment extends Fragment {
-
     private FragmentThirdBinding binding;
+
+    private EditText email;
+    private EditText password;
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
 
     @Override
     public View onCreateView(
@@ -20,12 +35,16 @@ public class ThirdFragment extends Fragment {
     ) {
 
         binding = FragmentThirdBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        email = view.findViewById(R.id.register_email);
+        password = view.findViewById(R.id.register_password);
 
         binding.backThird.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +57,23 @@ public class ThirdFragment extends Fragment {
         binding.confirmThird.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(ThirdFragment.this)
-                        .navigate(R.id.action_ThirdFragment_to_FourthFragment);
+
+                Boolean navigate = true;
+
+                if (isEmail(email) == false) {
+                    email.setError("Enter valid email!");
+                    navigate = false;
+                }
+
+                if (isEmpty(password)) {
+                    password.setError("Password is required!");
+                    navigate = false;
+                }
+
+                if (navigate) {
+                    NavHostFragment.findNavController(ThirdFragment.this)
+                            .navigate(R.id.action_ThirdFragment_to_FourthFragment);
+                }
             }
         });
     }
