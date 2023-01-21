e78781b582478f67e701e73afa5454970c5bee9d
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-01-21 23:14:45 +0000

    Result list fragment created

---
+++ app/src/main/java/com/beautyorder/androidclient/controller/main/FragmentResultList.java
@@ -0,0 +1,67 @@
+//
+//  FragmentResultList.java
+//
+//  Created by Mathieu Delehaye on 21/01/2023.
+//
+//  BeautyAndroid: An Android app to order and recycle cosmetics.
+//
+//  Copyright © 2022 Mathieu Delehaye. All rights reserved.
+//
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package com.beautyorder.androidclient.controller.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.beautyorder.androidclient.R;
+import com.beautyorder.androidclient.databinding.FragmentResultListBinding;
+
+public class FragmentResultList extends Fragment {
+    private FragmentResultListBinding mBinding;
+
+    @Override
+    public View onCreateView(
+        LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState
+    ) {
+        mBinding = FragmentResultListBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        var resultList = (ListView) view.findViewById(R.id.listView);
+        String[] listItem = getResources().getStringArray(R.array.array_technology);
+        var adapter = new ArrayAdapter<String>(getContext(),
+            android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        resultList.setAdapter(adapter);
+
+        resultList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String value=adapter.getItem(position);
+                Toast.makeText(getContext(),value,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+}
