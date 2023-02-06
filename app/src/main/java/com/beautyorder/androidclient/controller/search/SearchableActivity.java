ce85a7c7ea0c3e7107717e058802468dbe39b278
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-02-06 07:36:29 +0000

    Searchable activity added

---
+++ app/src/main/java/com/beautyorder/androidclient/controller/search/SearchableActivity.java
@@ -0,0 +1,41 @@
+//
+//  SearchableActivity.java
+//
+//  Created by Mathieu Delehaye on 30/01/2023.
+//
+//  BeautyAndroid: An Android app to order and recycle cosmetics.
+//
+//  Copyright © 2023 Mathieu Delehaye. All rights reserved.
+//
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+package com.beautyorder.androidclient.controller.search;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.Nullable;
+
+public class SearchableActivity extends Activity  {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the intent, verify the action and get the query
+        var intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d("BeautyAndroid", "mdl SearchableActivity::onCreate: query = " + query);
+        }
+    }
+}
