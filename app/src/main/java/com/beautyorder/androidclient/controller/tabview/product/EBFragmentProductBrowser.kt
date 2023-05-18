e9d49605e9d9b4dc0dcab2d4b51464b5714abb5c
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-05-18 07:29:12 +0100

    Product Browser view made more reusable

--- app/src/main/java/com/beautyorder/androidclient/controller/tabview/product/EBFragmentProductBrowser.kt
+++ app/src/main/java/com/beautyorder/androidclient/controller/tabview/product/EBFragmentProductBrowser.kt
@@ -21,22 +21,13 @@
 
 package com.beautyorder.androidclient.controller.tabview.product
 
-import androidx.compose.foundation.BorderStroke
+import android.app.Activity
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.android.java.androidjavatools.controller.tabview.product.FragmentProductBrowser
 import com.android.java.androidjavatools.controller.tabview.search.SearchBox
 import com.beautyorder.androidclient.R
@@ -47,10 +38,14 @@ class EBFragmentProductBrowser: FragmentProductBrowser() {
         browserView()
     }
 
+    override fun searchAndDisplayItems() {
+        TODO("Not yet implemented")
+    }
+
     @Composable
     fun browserView(
     ) {
-        var searchBox = SearchBox()
+        var searchBox = SearchBox(this.activity as Activity, this, null)
 
         val images = intArrayOf(R.drawable.beauty01, R.drawable.beauty02, R.drawable.beauty03,
             R.drawable.beauty04, R.drawable.beauty05)
@@ -78,72 +73,5 @@ class EBFragmentProductBrowser: FragmentProductBrowser() {
     @Composable
     fun previewBrowserView() {
         browserView()
-    }
-
-    @Composable
-    fun browserButton(
-        title: String
-        , color: Color
-    ) {
-        Button(
-            modifier = Modifier
-                .width(width = 188.dp)
-                .height(height = 60.dp)
-            , shape = RoundedCornerShape(size = 15.dp)
-            , border = BorderStroke(
-                1.dp
-                , Color.Black
-            )
-            , onClick = {
-            }
-            , colors = ButtonDefaults.buttonColors(
-                backgroundColor = color
-            )
-        ) {
-            Text(
-                text = title
-                , fontWeight = FontWeight.W400
-                , fontSize = 22.sp
-                , textAlign = TextAlign.Center
-            )
-        }
-    }
-
-    @Preview
-    @Composable
-    fun previewBrowserButton() {
-        browserButton("Free Samples", Color(0xFF3FA3BD))
-    }
-
-    @Composable
-    fun browserPager(
-        title: String
-        , images: IntArray)
-    {
-        Column {
-            Spacer(modifier = Modifier.height(5.dp))
-            Row {
-                Spacer(modifier = Modifier.width(25.dp))
-                Text(
-                    text = title
-                    , fontSize = 20.sp
-                    , fontWeight = FontWeight.Bold
-                    , modifier = Modifier
-                        .padding(all = 4.dp)
-                    , style = MaterialTheme.typography.h1
-                )
-            }
-            infinitePager(images)
-            Spacer(modifier = Modifier.height(10.dp))
-            Divider(color = Color.LightGray, thickness = 2.dp)
-        }
-    }
-
-    @Preview
-    @Composable
-    fun previewBrowserPager() {
-        val images = intArrayOf(R.drawable.beauty01, R.drawable.beauty02, R.drawable.beauty03,
-            R.drawable.beauty04, R.drawable.beauty05)
-        browserPager("Browse by Functions", images)
     }
 }
